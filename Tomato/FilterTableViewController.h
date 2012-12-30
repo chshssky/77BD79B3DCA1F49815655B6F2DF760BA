e33556f0f1e778a37b41e6329f7a646ad4a0d167
chshssky <chshssky@gmail.com>
2012-12-30 17:16:56 +0800

    改主界面左上Button为筛选，新增FilterModule和FilterTableViewController，

---
+++ Tomato/FilterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FilterTableViewController.h
+//  Tomato
+//
+//  Created by 崔 昊 on 12-12-30.
+//  Copyright (c) 2012年 Cui Hao. All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface FilterTableViewController : CoreDataTableViewController
+
+@end
---
+++ Tomato/FilterTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  FilterTableViewController.m
+//  Tomato
+//
+//  Created by 崔 昊 on 12-12-30.
+//  Copyright (c) 2012年 Cui Hao. All rights reserved.
+//
+
+#import "FilterTableViewController.h"
+
+@interface FilterTableViewController ()
+
+@end
+
+@implementation FilterTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
