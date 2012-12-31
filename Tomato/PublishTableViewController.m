04c72bd4b895974f6d669a2f4ccd96d53239c097
chshssky <chshssky@gmail.com>
2013-01-01 02:04:47 +0800

    push的接口 tag参数已按格式完成

--- Tomato/PublishTableViewController.m
+++ Tomato/PublishTableViewController.m
@@ -13,12 +13,13 @@
 
 @interface PublishTableViewController ()
 @property (weak, nonatomic) IBOutlet UIView *foodDetailView;
-
+@property (strong, nonatomic) NSMutableArray *tagArray;
 
 @end
 
 @implementation PublishTableViewController
 @synthesize foodDetailView = _foodDetailView;
+@synthesize tagArray = _tagArray;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -36,6 +37,11 @@
     [self setupFetchedResultController];
     self.title = @"发布";
     //[self.tableView setTableHeaderView:self.headerView];
+    self.tagArray = [[NSMutableArray alloc] init];
+    for (int i = 0; i < [self.fetchedResultsController.fetchedObjects count]; i++) {
+        [self.tagArray addObject:[NSNumber numberWithBool:NO]];
+    }
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,7 +58,7 @@
     self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:requst
                                                                 managedObjectContext:self.managedObjectContext
                                                                           sectionNameKeyPath:@"tagType"
-                                                                                   cacheName:nil];
+                                                                                   cacheName:nil];    
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -73,22 +79,30 @@
     if (indexPath.section == 0) {
         if ([tableView cellForRowAtIndexPath:indexPath].accessoryType != UITableViewCellAccessoryCheckmark) {
             [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryCheckmark;
+            self.tagArray[indexPath.row] = [NSNumber numberWithBool:YES];
+            NSLog(@"%d", indexPath.row);
         } else {
             [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryNone;
+            self.tagArray[indexPath.row] = [NSNumber numberWithBool:NO];
+
         }
         for (NSInteger i = 0; i < [self.tableView numberOfRowsInSection:0]; i ++) {
             if (i != indexPath.row) {
                 [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:i inSection:indexPath.section]].accessoryType = UITableViewCellAccessoryNone;
+                self.tagArray[i] = [NSNumber numberWithBool:NO];
             }
         }
     } else {
     
-    if ([tableView cellForRowAtIndexPath:indexPath].accessoryType != UITableViewCellAccessoryCheckmark) {
-        [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryCheckmark;
-    } else {
-        [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryNone;
-    }
-    
+        if ([tableView cellForRowAtIndexPath:indexPath].accessoryType != UITableViewCellAccessoryCheckmark) {
+            [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryCheckmark;
+            self.tagArray[3 + indexPath.section] = [NSNumber numberWithBool:YES];
+            NSLog(@"%d", 3 + indexPath.section);
+        } else {
+            [tableView cellForRowAtIndexPath:indexPath].accessoryType = UITableViewCellAccessoryNone;
+            self.tagArray[3 + indexPath.section] = [NSNumber numberWithBool:NO];
+            NSLog(@"%d", 3 + indexPath.section);
+        }
     }
     
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
@@ -96,7 +110,15 @@
 
 - (NSString *)getSelectedTagsFromTableView
 {
-    
+    NSString *tagDes = @"";
+    int i = 1;
+    for (NSNumber *tagBool in self.tagArray) {
+        if ([tagBool boolValue]) {
+            tagDes = [tagDes stringByAppendingFormat:@"&%d", i];
+        }
+        i++;
+    }
+    return [tagDes substringFromIndex:1];
 }
 
 - (IBAction)completeButtonPushed:(UIBarButtonItem *)sender
@@ -107,11 +129,11 @@
     
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"foodImage" ofType:@"png"];
     UIImage *image = [[UIImage alloc] initWithContentsOfFile:filePath];
-    
-    NSString *path = [NetworkInterface generateRandomString:15];
-    [NetworkInterface PublishFood:foodDetail.foodNameTextField.text foodprice:foodDetail.foodPriceTextField.text publishtime:@"2012-10-20 23:20:19" foodimgname:path restaurantname:@"KFC" tagsname:@"1&4&6"];
-    NSLog(@"%@", foodDetail.foodImageDetail.currentBackgroundImage);
-    [NetworkInterface UploadImage:foodDetail.foodImageDetail.currentBackgroundImage picturename:path];
+    NSLog(@"%@", [self getSelectedTagsFromTableView]);
+//    NSString *path = [NetworkInterface generateRandomString:15];
+//    [NetworkInterface PublishFood:foodDetail.foodNameTextField.text foodprice:foodDetail.foodPriceTextField.text publishtime:@"2012-10-20 23:20:19" foodimgname:path restaurantname:@"KFC" tagsname:@"1&4&6"];
+//    NSLog(@"%@", foodDetail.foodImageDetail.currentBackgroundImage);
+//    [NetworkInterface UploadImage:foodDetail.foodImageDetail.currentBackgroundImage picturename:path];
     
 }
 
